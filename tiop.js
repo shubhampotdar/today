4d14eb47608261c946bf748de8dbe0a27cc45412
shubhampotdar <shubhampotdar14081998@gmail.com>
2017-10-20 14:46:33 +0530

    opijohyfg
    ;lkljkhgf

---
+++ tiop.js
@@ -0,0 +1,37 @@
+new Tippy('#ii1',{
+    		position:'top',
+    		animation:'fade',
+            interactive:'true',
+            size:'big',
+            arrow: true,
+            arrowSize:'big'
+    	});
+
+new Tippy('#two',{
+    		position:'top',
+    		animation:'fade',
+            interactive:'true',
+            size:'big',
+            arrow: true,
+            arrowSize:'big'
+    	});
+new Tippy('#three',{
+    		position:'top',
+    		animation:'fade',
+            interactive:'true',
+            size:'big',
+            arrow: true,
+            arrowSize:'big'
+    	});
+new Tippy('#four',{
+    		position:'top',
+    		animation:'fade',
+            interactive:'true',
+            size:'big',
+            arrow: true,
+            arrowSize:'big'
+    	});
+
+$('a').click(function(){
+    $(this).addClass("visited");
+});
